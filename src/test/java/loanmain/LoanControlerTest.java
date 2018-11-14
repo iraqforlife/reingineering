991af8af4ad2750ad546decc6c374ca4821321f7
an27450 <eugeni.mironov.1@ens.etsmtl.ca>
2018-11-14 17:44:34 -0500

    Tests

--- src/test/java/loanmain/LoanControlerTest.java
+++ src/test/java/loanmain/LoanControlerTest.java
@@ -8,56 +8,81 @@ import static org.junit.Assert.*;
 
 public class LoanControlerTest {
 
-
     @Mocked
     private EventBus eb;
-    @Injectable
-    private EventBus eb2;
 
-    @Mocked
-    private LoanControler lc;
+//    @Mocked
+//    private LoanControler lc;
+
     @Mocked
     private CalcLoanItem calc;
 
-
     @Test
     public void testUpdateOptionDuree() {
-//        MockUp<CalcLoanItem> cli = new MockUp<CalcLoanItem>()
-//        {
-//            @Mock
-//            public Double computeDuration(final LoanItem pItem) {
-//                if (pItem.getMensualite().equals(0F) || pItem.getTaux().equals(0F) || pItem.getAmount().equals(0F)) {
-//                    return null;
-//                }
-//                double lMens = pItem.getMensualite() - (pItem.getAmount() * pItem.getInsurance() / 1200D);
-//                double lTaux = pItem.getTaux() / 1200D;
-//                return -Math.log(1D - pItem.getAmount() * lTaux / lMens) / Math.log(1D + lTaux) / 12D;
-//            }
-//        };
 
         LoanItem item = new LoanItem();
+        LoanControler lc = new LoanControler(eb);
         lc.setCurrentItem(item);
         lc.setLoanType(LoanItem.LoanType.DUREE);
         lc.updateEntry((float)10,(float)10,(float)10,(float)10);
-//        new Verifications() {{
-//            calc.computeDuration(lc.getCurrentItem());times=1;
-//           eb.post(any); times =1;
-//        }};
 
-
-
-        assertTrue(true);
+        new Verifications() {{
+            calc.computeDuration(lc.getCurrentItem());times=1;
+            calc.computeMensHorsAss(lc.getCurrentItem());times=0;
+            calc.computeAmount(lc.getCurrentItem());times=0;
+            calc.computeRate(lc.getCurrentItem());times=0;
+            eb.post(any); times =1;
+        }};
     }
 
-//    @Test
-//    public void testUpdateOptionMensualite() {
-//    }
-//
-//    @Test
-//    public void testUpdateOptionMontant() {
-//    }
-//
-//    @Test
-//    public void testUpdateOptionTaux() {
-//    }
+    @Test
+    public void testUpdateOptionMensualite() {
+        LoanItem item = new LoanItem();
+        LoanControler lc = new LoanControler(eb);
+        lc.setCurrentItem(item);
+        lc.setLoanType(LoanItem.LoanType.MENSUALITE);
+        lc.updateEntry((float)10,(float)10,(float)10,(float)10);
+
+        new Verifications() {{
+            calc.computeDuration(lc.getCurrentItem());times=0;
+            calc.computeMensHorsAss(lc.getCurrentItem());times=1;
+            calc.computeAmount(lc.getCurrentItem());times=0;
+            calc.computeRate(lc.getCurrentItem());times=0;
+            eb.post(any); times =1;
+        }};
+    }
+
+    @Test
+    public void testUpdateOptionMontant() {
+        LoanItem item = new LoanItem();
+        LoanControler lc = new LoanControler(eb);
+        lc.setCurrentItem(item);
+        lc.setLoanType(LoanItem.LoanType.MONTANT);
+        lc.updateEntry((float)10,(float)10,(float)10,(float)10);
+
+        new Verifications() {{
+            calc.computeDuration(lc.getCurrentItem());times=0;
+            calc.computeMensHorsAss(lc.getCurrentItem());times=0;
+            calc.computeAmount(lc.getCurrentItem());times=1;
+            calc.computeRate(lc.getCurrentItem());times=0;
+            eb.post(any); times =1;
+        }};
+    }
+
+    @Test
+    public void testUpdateOptionTaux() {
+        LoanItem item = new LoanItem();
+        LoanControler lc = new LoanControler(eb);
+        lc.setCurrentItem(item);
+        lc.setLoanType(LoanItem.LoanType.TAUX);
+        lc.updateEntry((float)10,(float)10,(float)10,(float)10);
+
+        new Verifications() {{
+            calc.computeDuration(lc.getCurrentItem());times=0;
+            calc.computeMensHorsAss(lc.getCurrentItem());times=0;
+            calc.computeAmount(lc.getCurrentItem());times=0;
+            calc.computeRate(lc.getCurrentItem());times=1;
+            eb.post(any); times =1;
+        }};
+    }
 }
